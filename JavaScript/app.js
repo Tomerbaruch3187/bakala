80620303b3f944119a6056714722889ea8667422
Tomer <tomerbaruch4444@gmail.com>
2022-06-07 15:52:46 +0300

    LessonNumberOne

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -13,21 +13,22 @@
 
 
 function addTax(price) {
-    if (!price || price <= 0 || typrof(price) === 'string') {
+    if (!price || price <= 0 || typeof (price) === 'string') {
         throw Error('Nothing is for free! please supply a price');
     }
 
-    const vat = 1.17; //maam
-    const total = (price * vat).toFixed(1);
+    const vat = 1.17;
+    const total = (price * vat).toFixed(2);
     return total;
 }
 
 function generateId() {
     // const rand = Math.random();
-    // console.log(` rand: ${rand}`);
-    // const num = rand * 10000;
-    // console.log(` num: ${rand}`);
+    // console.log(`rand: ${rand}`);
+    // const num = rand * 100000;
+    // console.log(`num: ${num}`);
     // const floor = Math.floor(num);
+    // console.log(`floor: ${floor}`);
     // return floor;
 
     const time = new Date().getTime();
