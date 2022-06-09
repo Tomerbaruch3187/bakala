5c02c75a8cda12b9dfe8a2c36367f7155589b3fa
Tomer <tomerbaruch4444@gmail.com>
2022-06-09 16:24:51 +0300

    changes

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -9,47 +9,57 @@ function addTax(price) {
 }
 
 function generateId() {
-    // const rand = Math.random();
-    // console.log(`rand: ${rand}`);
-    // const num = rand * 100000;
-    // console.log(`num: ${num}`);
-    // const floor = Math.floor(num);
-    // console.log(`floor: ${floor}`);
-    // return floor;
-
     const time = new Date().getTime();
     return Math.floor(Math.random() * time);
 }
 
 const products = [{
-        id: 1,
+        id: generateId(),
         price: 15,
         name: 'yello cheese',
-        image: 'https://cdn.pixabay.com/photo/2016/03/05/19/24/cheese-1238395_960_720.jpg',
+        image: '2016/03/05/19/24/cheese-1238395_960_720.jpg',
         description: 'great product really'
     },
     {
-        id: 2,
+        id: generateId(),
         price: 3,
         name: 'ice cream',
-        image: 'https://cdn.pixabay.com/photo/2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
+        image: '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
         description: 'be careful - very cold'
     },
 ];
 
+function filterProducts() {
+
+}
+
+
+function getProductsTemplate() {
+    const IMG_PATH = 'https://cdn.pixabay.com/photo/'; // configuration of the project
+    let html = '';
+
+    products.forEach(prod => {
+        html +=
+            `<div class="card" style="width: 18rem;">
+                <img src="${IMG_PATH}${prod.image}"
+                    class="card-img-top" alt="${prod.description}">
+                <div class="card-body">
+                    <h5 class="card-title">${prod.name}</h5>
+                    <p class="card-text">$
+                        ${addTax(prod.price)}</p>
+                    <a href="#" class="btn btn-primary">View Details</a>
+                </div>
+            </div>`;
+    })
+
+    return html;
+}
+
+
 function displayProduct() {
     const container = document.getElementById('products-container');
-    products.forEach(product => {
-
-        container.innerHTML += `<div class="card" style="width: 18rem;">
-    <img src="${product.image}"
-    class="card-img-top" alt="${product.description}">
-    <div class="card-body">
-    <h5 class="card-title">${product.name}</h5>
-    <p class="card-text">$${product.price}</p>
-    <a href="#" class="btn btn-primary">View Details</a>
-    </div>
-    </div>`;
-    });
+    const html = getProductsTemplate();
+    container.innerHTML = html;
 }
+
 displayProduct();
