178a107993d7a9cb29755977976dd1520391e5ca
Tomer <tomerbaruch4444@gmail.com>
2022-06-09 11:30:41 +0300

    jsUpdate

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,17 +1,3 @@
-// function addTax(price) {
-//     if (price > 0) {
-//         const vat = 1.17; //maam
-//         const total = (price * vat).toFixed(1);
-//         return total;
-//     } else {
-//         return 'Nothing is for free! please supply a price';
-//     }
-// }
-
-
-
-
-
 function addTax(price) {
     if (!price || price <= 0 || typeof (price) === 'string') {
         throw Error('Nothing is for free! please supply a price');
@@ -23,6 +9,47 @@ function addTax(price) {
 }
 
 function generateId() {
+    // const rand = Math.random();
+    // console.log(`rand: ${rand}`);
+    // const num = rand * 100000;
+    // console.log(`num: ${num}`);
+    // const floor = Math.floor(num);
+    // console.log(`floor: ${floor}`);
+    // return floor;
+
     const time = new Date().getTime();
     return Math.floor(Math.random() * time);
 }
+
+const products = [{
+        id: 1,
+        price: 15,
+        name: 'yello cheese',
+        image: 'https://cdn.pixabay.com/photo/2016/03/05/19/24/cheese-1238395_960_720.jpg',
+        description: 'great product really'
+    },
+    {
+        id: 2,
+        price: 3,
+        name: 'ice cream',
+        image: 'https://cdn.pixabay.com/photo/2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
+        description: 'be careful - very cold'
+    },
+];
+
+function displayProduct() {
+    const container = document.getElementById('products-container');
+    products.forEach(product => {
+
+        container.innerHTML += `<div class="card" style="width: 18rem;">
+    <img src="${product.image}"
+    class="card-img-top" alt="${product.description}">
+    <div class="card-body">
+    <h5 class="card-title">${product.name}</h5>
+    <p class="card-text">$${product.price}</p>
+    <a href="#" class="btn btn-primary">View Details</a>
+    </div>
+    </div>`;
+    });
+}
+displayProduct();
